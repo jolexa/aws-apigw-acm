3d8498868bdd55bdce0753871dbe2a91ec7ac91f
Jeremy Olexa <jolexa@jolexa.net>
2017-06-29 20:09:11 -0500

    Output Region in body

--- lambda/hello-js.js
+++ lambda/hello-js.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
     callback(null, {
         "statusCode": 200,
-        "body": "hello world, from node\n"
+        "body": "hello world, from node. Region: " + process.env.AWS_DEFAULT_REGION + "\n"
     });
 }
--- lambda/hello.py
+++ lambda/hello.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 
+import os
+
 def handler(event, context):
-    return { "statusCode": 200, "body": "hello world, from python\n" }
+    return { "statusCode": 200, "body": "hello world, from python. Region: " +
+            os.environ['AWS_DEFAULT_REGION'] + "\n" }
